4e76c5a932a2590cf3142b4187f8acb84fc021d3
Carlos Ferreira <carlosferreirahd@gmail.com>
2023-02-04 16:42:04 -0300

    feat: fetching consumers list

--- app/screens/Home/ConsumersTab.jsx
+++ app/screens/Home/ConsumersTab.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { View } from 'react-native';
+import { useErrorHandler } from '../../hooks/useErrorHandler';
+import { useQuery } from '../../hooks/useQuery';
+import { SELECT_ALL_CONSUMERS } from '../../utils/queries';
+import { isNilOrEmpty } from '../../utils/verifications';
 
 export function ConsumersTab() {
+
+  const { showErrorModal } = useErrorHandler();
+
+  const { loading, error, data, executeQuery } = useQuery();
+  const consumers = data?.rows?._array;
+  console.log({ loading, error, data, consumers });
+
+  const getAllConsumers = useCallback(() => {
+    executeQuery({
+      query: SELECT_ALL_CONSUMERS,
+      params: [],
+    });
+  }, [executeQuery]);
+
+  useEffect(() => {
+    getAllConsumers();
+  }, [getAllConsumers]);
+
+  useEffect(() => {
+    if (!isNilOrEmpty(error)) {
+      showErrorModal({
+        title: 'Erro ao buscar clientes',
+        description: 'Não foi possível buscar a lista de clientes no banco de dados',
+        buttonText: 'Tentar novamente',
+        onButtonClick: getAllConsumers,
+      });
+    }
+  }, [error]);
 
   return (
     <View>
---
+++ app/utils/queries.js
@@ -0,0 +1,1 @@
+export const SELECT_ALL_CONSUMERS = "SELECT * FROM consumers;";
