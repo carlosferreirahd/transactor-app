1ea9c3bb7125702a5ed696e0f4b2b4f0003e6a43
Carlos Ferreira <carlosferreirahd@gmail.com>
2023-02-04 16:19:13 -0300

    feat: creating query to handle sql queries and app state

---
+++ app/hooks/useQuery.js
@@ -0,0 +1,60 @@
+import { useCallback, useState } from 'react';
+import { useDatabase } from './useDatabase';
+import { isNilOrEmpty } from '../utils/verifications';
+
+export function useQuery() {
+  const { db } = useDatabase();
+
+  const [queryState, setQueryState] = useState({
+    loading: false,
+    error: null,
+    data: null,
+  });
+
+  if (isNilOrEmpty(db)) {
+    throw new Error('Invalid database object');
+  }
+
+  const queryRunner = useCallback(async ({ query, params }) => {
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          query,
+          [...params],
+          (tx, result) => {
+            resolve(result);
+          },
+          (tx, err) => {
+            reject(err);
+          },
+        );
+      });
+    });
+  }, []);
+
+  const executeQuery = useCallback(({ query, params }) => {
+    setQueryState(prevState => ({
+      ...prevState,
+      loading: true,
+    }));
+
+    queryRunner({ query, params })
+      .then(res => setQueryState(prevState => ({
+        ...prevState,
+        data: res,
+      })))
+      .catch(err => setQueryState(prevState => ({
+        ...prevState,
+        error: err,
+      })))
+      .finally(() => setQueryState(prevState => ({
+        ...prevState,
+        loading: false,
+      })));
+  }, [queryRunner]);
+
+  return {
+    ...queryState,
+    executeQuery,
+  };
+}
