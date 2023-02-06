828323e695b67ec0c8aaf7aa3a5010d433ac5355
Carlos Ferreira <carlosferreirahd@gmail.com>
2023-02-06 20:34:42 -0300

    fix: fixing transactions EmptyState text

--- app/screens/Transactions/TransactionsDetails.jsx
+++ app/screens/Transactions/TransactionsDetails.jsx
@@ -43,7 +43,7 @@ export function TransactionsDetails({
       return (
         <EmptyState
           icon="currency-usd-off"
-          description="Adicione transações com o botão abaixo"
+          description={`Adicione transações com o botão "+" acima`}
         />
       );
     }
